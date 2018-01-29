bc8b2a4bf26eeb99a3a1281a8dcec9719c5e30c9
Terry Chun <docterry@hotmail.com>
2018-01-28 19:48:50 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="icon" type="image/png" href="favicon.png" />
+    <link rel="apple-touch-icon" href="favicon.png" />
+    <link href="" rel="apple-touch-startup-image" />
+    <title>Paging v4</title>
+</head>
+    <body>
+        <?php
+        ?>
+    </body>
+</html>
